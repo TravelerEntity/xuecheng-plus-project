5172569e35441d41c53144524312df7934bd59b7
leechee <lee.bamboo@outlook.com>
2024-04-11 22:35:07 +0800

    feat: 使用 nacos，抽取配置文件

--- xuecheng-plus-content/xuecheng-plus-api/src/main/java/com/xuecheng/content/api/CourseCategoryController.java
+++ xuecheng-plus-content/xuecheng-plus-api/src/main/java/com/xuecheng/content/api/CourseCategoryController.java
@@ -2,6 +2,8 @@ package com.xuecheng.content.api;
 
 import com.xuecheng.content.model.dto.CourseCategoryTreeDto;
 import com.xuecheng.content.service.CourseCategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ import java.util.List;
  * Created by Lee Yian on 2024/3/31 18:08
  */
 @Slf4j
+@Api(value = "课程分类接口", tags = "课程分类接口")
 @RestController
 public class CourseCategoryController {
     @Autowired
@@ -20,6 +23,7 @@ public class CourseCategoryController {
 
 
     @GetMapping("/course-category/tree-nodes")
+    @ApiOperation("按照树形结构获取课程分类")
     public List<CourseCategoryTreeDto> queryTreeNodes() {
         return         courseCategoryService.queryTreeNodes("1") ;
     }
