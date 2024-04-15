e583385575ea6ff28b44008c55fa1e2eea62916b
leechee <lee.bamboo@outlook.com>
2024-04-15 21:43:57 +0800

    feat: 完成网关工程、完成媒资工程的图片上传

---
+++ xuecheng-plus-media/xuecheng-plus-media-model/src/main/java/com/xuecheng/media/model/dto/UploadFileResultDto.java
@@ -0,0 +1,13 @@
+package com.xuecheng.media.model.dto;
+
+import com.xuecheng.media.model.po.MediaFiles;
+import lombok.Data;
+
+/**
+ * 文件上传接口的响应 DTO
+ */
+
+@Data
+public class UploadFileResultDto extends MediaFiles {
+
+}
